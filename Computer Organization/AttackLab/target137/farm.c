a42ca016953d828576ec8a1ed222bebb8190f558
Erdem Şamlıoğlu <e2448843@ceng.metu.edu.tr>
2025-01-02 15:07:00 +0300

    Added Computer Organization folder with BombLab and AttackLab

---
+++ Computer Organization/AttackLab/target137/farm.c
@@ -0,0 +1,111 @@
+/* This function marks the start of the farm */
+int start_farm()
+{
+    return 1;
+}
+
+unsigned getval_416()
+{
+    return 2428995912U;
+}
+
+unsigned getval_274()
+{
+    return 3287976264U;
+}
+
+unsigned addval_150(unsigned x)
+{
+    return x + 2463533384U;
+}
+
+unsigned addval_377(unsigned x)
+{
+    return x + 3254863977U;
+}
+
+void setval_482(unsigned *p)
+{
+    *p = 2429978696U;
+}
+
+unsigned getval_288()
+{
+    return 2428995912U;
+}
+
+void setval_269(unsigned *p)
+{
+    *p = 3355085128U;
+}
+
+void setval_264(unsigned *p)
+{
+    *p = 2425387016U;
+}
+
+unsigned getval_378()
+{
+    return 3284633672U;
+}
+
+void setval_225(unsigned *p)
+{
+    *p = 2425379052U;
+}
+
+unsigned getval_473()
+{
+    return 3599321250U;
+}
+
+void setval_141(unsigned *p)
+{
+    *p = 2432338252U;
+}
+
+unsigned getval_373()
+{
+    return 3284271433U;
+}
+
+void setval_497(unsigned *p)
+{
+    *p = 3263252545U;
+}
+
+unsigned addval_476(unsigned x)
+{
+    return x + 3347671209U;
+}
+
+unsigned getval_474()
+{
+    return 2425393232U;
+}
+
+unsigned addval_486(unsigned x)
+{
+    return x + 4203301119U;
+}
+
+unsigned addval_138(unsigned x)
+{
+    return x + 3285616968U;
+}
+
+unsigned addval_168(unsigned x)
+{
+    return x + 2421726877U;
+}
+
+void setval_387(unsigned *p)
+{
+    *p = 3254863911U;
+}
+
+/* This function marks the end of the farm */
+int end_farm()
+{
+    return 1;
+}
